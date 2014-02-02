03048665badeb21cb674ada954ae1390c4ef7350
Rob Collins <rob@clockworks.ie>
2014-02-02 03:14:18 +0000

    added python earthquake mag puller

---
+++ jsondata_finished.py
@@ -0,0 +1,25 @@
+# 
+# Example file for parsing and processing JSON
+#
+
+import urllib2
+import json
+
+def printResults(data):
+  theJSON = json.loads(data)
+
+  for i in theJSON["features"]:
+    if i["properties"]["mag"] >= 1.0:
+      print "%2.1f" % i["properties"]["mag"]
+
+def main():
+  urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+  webUrl = urllib2.urlopen(urlData)
+  if (webUrl.getcode() == 200):
+    data = webUrl.read()
+    printResults(data)
+  else:
+    print "Received an error from server, cannot retrieve results " + str(webUrl.getcode())
+
+if __name__ == "__main__":
+  main()
